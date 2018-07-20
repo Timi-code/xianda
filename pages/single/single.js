bfdd1cf30abbcce0a24e8653aa90ed904ed5867c
tangchengli <tcl123>
2018-07-20 18:06:24 +0800

    update

--- pages/single/single.js
+++ pages/single/single.js
@@ -148,18 +148,21 @@ Page({
   },
 
   addSingle: function(e) {
-    wx.chooseImage({
-      count: 1, // 默认9
-      sizeType: ['original', 'compressed'], // 可以指定是原图还是压缩图，默认二者都有
-      sourceType: ['album'], // 可以指定来源是相册还是相机，默认二者都有
-      success: function (res) {
-        // 返回选定照片的本地文件路径列表，tempFilePath可以作为img标签的src属性显示图片
-        console.log(res);
-        var tempFilePaths = res.tempFilePaths
-        wx.navigateTo({
-          url: `/pages/editor-img/editor-img?path=${tempFilePaths}`,
-        })
-      }
+    wx.navigateTo({
+      url: '/pages/edit-single/edit-single',
     })
+    // wx.chooseImage({
+    //   count: 1, // 默认9
+    //   sizeType: ['original', 'compressed'], // 可以指定是原图还是压缩图，默认二者都有
+    //   sourceType: ['album'], // 可以指定来源是相册还是相机，默认二者都有
+    //   success: function (res) {
+    //     // 返回选定照片的本地文件路径列表，tempFilePath可以作为img标签的src属性显示图片
+    //     console.log(res);
+    //     var tempFilePaths = res.tempFilePaths
+    //     wx.navigateTo({
+    //       url: `/pages/editor-img/editor-img?path=${tempFilePaths}`,
+    //     })
+    //   }
+    // })
   }
 })
